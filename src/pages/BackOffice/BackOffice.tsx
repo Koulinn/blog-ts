3f807e60c7baa5cc9bb5d0732344f12a0a5d981f
Rafael Lima <rafauxdev@gmail.com>
2022-04-25 18:31:20 +0100

    Call components to the BackOffice

--- src/pages/BackOffice/BackOffice.tsx
+++ src/pages/BackOffice/BackOffice.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import BackOfficeForm from "../../components/BackOfficeForm/BackOfficeForm";
+import ContentLayout from "../../components/shareable_components/ContentLayout";
 
 function BackOffice() {
-  return <div>BackOffice</div>;
+  return (
+    <ContentLayout>
+      <BackOfficeForm />
+    </ContentLayout>
+  );
 }
 
 export default BackOffice;

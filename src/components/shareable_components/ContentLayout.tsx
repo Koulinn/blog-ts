f5afb9ad7c940af15553945f7ad104898b7321ff
Rafael Lima <rafauxdev@gmail.com>
2022-04-25 18:32:48 +0100

    Create ContentLayout component

---
+++ src/components/shareable_components/ContentLayout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container, Grid } from "@mui/material";
+interface AuxProps {
+  children:
+    | React.ReactChild
+    | React.ReactChild[]
+    | React.ReactChildren
+    | React.ReactChildren[];
+}
+
+function ContentLayout({ children }: AuxProps) {
+  return (
+    <Container
+      sx={{
+        backgroundColor: "#dfdfdf4d",
+        padding: "64px",
+        borderRadius: "24px",
+      }}
+    >
+      <Grid>{children}</Grid>
+    </Container>
+  );
+}
+
+export default ContentLayout;

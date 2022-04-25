aec2c6c48f7507c49a81c214b1dcd1cbcaefeac2
Rafael Lima <rafauxdev@gmail.com>
2022-04-25 18:30:38 +0100

    Add Layout

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,24 +1,21 @@
-import React from "react";
-import { Container, Grid } from "@mui/material";
 import Post from "../../components/Post/Post";
+import ContentLayout from "../../components/shareable_components/ContentLayout";
 
 function Blog() {
   return (
-    <Container>
-      <Grid>
-        <Post
-          author="Rafa Lima"
-          avatar="https://res.cloudinary.com/koulin/image/upload/v1629203268/sample.jpg"
-          title="Cake pls"
-          text="kjsadh akjsdhkla hahjd kjashkjash djkahsdkah "
-          imageAlt="fdsf"
-          imageUrl="https://res.cloudinary.com/koulin/image/upload/v1629203268/sample.jpg"
-          createdAt="654 54 654"
-          tags={["cake", "sad", "fasddfas"]}
-          likes={["asdasd", "asdasdas"]}
-        />
-      </Grid>
-    </Container>
+    <ContentLayout>
+      <Post
+        author="Rafa Lima"
+        avatar="https://res.cloudinary.com/koulin/image/upload/v1629203268/sample.jpg"
+        title="Cake pls"
+        text="kjsadh akjsdhkla hahjd kjashkjash djkahsdkah "
+        imageAlt="fdsf"
+        imageUrl="https://res.cloudinary.com/koulin/image/upload/v1629203268/sample.jpg"
+        createdAt="654 54 654"
+        tags={["cake", "sad", "fasddfas"]}
+        likes={["asdasd", "asdasdas"]}
+      />
+    </ContentLayout>
   );
 }
 
